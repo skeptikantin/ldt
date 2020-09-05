5f6c45ad2c9d87d37e0f6bf8363ee88e372f592a
Susanne Flach <susanne.flach@unine.ch>
2020-09-05 08:30:09 +0200

    Update

--- data_includes/main.js
+++ data_includes/main.js
@@ -5,7 +5,7 @@
 
 // Header at the beginning of each trial
 Header(
-    // use globar variable element to store participants' name
+    // use global variable element to store participants' name
     newVar("ParticipantName")
         .global()
     // delay of 500ms before every trial
@@ -13,10 +13,19 @@ Header(
         .start()
         .wait()
 )
+// Log the participant
 .log("Name", getVar("ParticipantName"))
 
 newTrial("intro",
-    newText(<p>))
+
+    newText("<p>Welcome to this lexical decision task.")
+        .css("font-size", "1.5em")
+        .css("font-family", "Verdana")
+        .center()
+        .print()
+)
+
+
 
 Template("training.csv", row =>
     newTrial("training",
